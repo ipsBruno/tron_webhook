a5e64bab43c418469a2cb6a6462db0d280013fac
Bruno da Silva <email@brunodasilva.com>
2023-06-17 10:55:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,36 +19,27 @@ const axios = require('axios');
 const createKeccakHash = require('keccak')
 const secp256k1 = require('secp256k1')
 
-// Quantos endereços derivar da seed
-const addressToView = 10000
-
-// Qual bloco começa analisar
+const addressToView = 100000
 const initialBlock = 52128086
-
-// Contrato USDT
 const contractAddress = 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t';
-
-const seed =  'your seed here please lol'
+const seed =  'you seed here please'
 
 
 const utils = hdWallet.utils
 var addressesToAnalyse = []
 
-// Aqui vai derivar todos endereços e salvar na memória do aplicativo
+
 async function saveAddressToMemory(){ 
     for(var wallet = 0; wallet < addressToView; wallet++) {
         let account = await utils.getAccountAtIndex(seed, wallet);
         addressesToAnalyse.push(account.address)
     }
-    console.log(addressesToAnalyse)
     setInterval(updateLatestBlock, 3000)
     setTimeout(receiveBlock, 1200)
 }
 
 
 saveAddressToMemory()
-
-
 
 
 
@@ -61,18 +52,61 @@ const tronGrid = new TronGrid(tronWeb)
 var currentBlock = getLatestLocalBlock()
 var latestBlockOnBlockchain = currentBlock
 
-// Função para pegar último bloco do disco
+
+
+
+
+
+
+async function getContractEvent(block, fingerprint) {
+    let requestObj = {
+        block_number: block,
+        event_name: "Transfer",
+        only_confirmed:true,
+        limit: 200,
+        order_by: "timestamp,desc"
+    }
+    if(fingerprint) {
+        requestObj.fingerprint = fingerprint
+    }
+    let result = await tronGrid.contract.getEvents(contractAddress, requestObj)
+
+    result.data = result.data.map(tx => {
+        tx.result.to_address = tronWeb.address.fromHex(tx.result.to)
+        tx.result.from_address = tronWeb.address.fromHex(tx.result.from)
+        return tx
+    })
+    for (var i in result.data) {
+        if (result.data[i].caller_contract_address != contractAddress) continue
+        if (result.data[i].event_name != 'Transfer') continue
+        onReceiveTransfer(result.data[i].transaction_id, result.data[i].result.value, result.data[i].result.to_address)
+    }
+
+    if(result.meta && result.meta.links && result.meta.links.next) {
+        console.log('O bloco: ', currentBlock, ' tem mais de 1 evento .. requisitando novamente', result)
+        return getContractEvent(block, result.meta.fingerprint)
+    }
+}
+
+
+
 function getLatestLocalBlock() {
-    if (!fs.existsSync('latestBlock.log')) return initialBlock
+    if (!fs.existsSync('latestBlock.log')) {
+        console.log('Sem bloco inicial.. Começando do zero')
+        return initialBlock
+    }
     return fs.readFileSync('latestBlock.log').toString()
 }
 
-// Funcao para salvar bloco atual no disco
 function setLatestLocalBlock(number) {
-    return fs.writeFileSync('latestBlock.log', number.toString())
+    try {
+        fs.writeFileSync('latestBlock.log', number.toString())
+    }
+    catch(e) {
+        console.log(e)
+    }
 }
 
-// Funcao para ler o bloco atual
 function updateLatestBlock() {
     request({
         method: 'GET',
@@ -89,45 +123,28 @@ function updateLatestBlock() {
 }
 
 
-
 async function receiveBlock() {
     if (currentBlock >= latestBlockOnBlockchain) {
         return setTimeout(receiveBlock, 250)
     }
     console.log('Bloco: ', currentBlock, ' de ', latestBlockOnBlockchain)
     try {
-        var result = await tronGrid.contract.getEvents(contractAddress, {
-            block_number: currentBlock,
-            event_name: "Transfer",
-            only_confirmed:true,
-            limit: 200,
-            order_by: "timestamp,asc"
-        })
-        result.data = result.data.map(tx => {
-            tx.result.to_address = tronWeb.address.fromHex(tx.result.to)
-            tx.result.from_address = tronWeb.address.fromHex(tx.result.from)
-            return tx
-        })
-        for (var i in result.data) {
-            if (result.data[i].caller_contract_address != contractAddress) continue
-            if (result.data[i].event_name != 'Transfer') continue
-            onReceiveTransfer(result.data[i].transaction_id, result.data[i].result.value, result.data[i].result.to_address)
-        }
+        getContractEvent(currentBlock)
         currentBlock++
-        setLatestLocalBlock(currentBlock)
+        setLatestLocalBlock(currentBlock) 
     } catch (e) {
-        return setTimeout(receiveBlock, 1250)
+        return setTimeout(receiveBlock, 2000)
     }
     return setTimeout(receiveBlock, 250)
 }
 
 
 
+/* THIS FUNCTION IS CALLED EVERY TRANSACTION ON CONTRACT */
 async function onReceiveTransfer(txid, valor, toaddress) {
-    // Aqui importante.. Verifica se o endereço que movimentou é nosso endereço
     if(addressesToAnalyse.includes(toaddress)) {
-        console.log('O endereço recebeu uma transação: ' , txid, ' | Valor: ', valor, ' | TXID: ', txid);
-        await axios.get(`https://api.yourwebhook.com/Webbook/${txid}/${valor}/${txid}`)                    
+        console.log('O endereço recebeu uma transação: ' , toaddress, ' | Valor: ', valor, ' | TXID: ', txid);
+        await axios.get(`https://api.yourwebhook.com/${toaddress}/${valor}/${txid}`)                    
     }
 }
 
